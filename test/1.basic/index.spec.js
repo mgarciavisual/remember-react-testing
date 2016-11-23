9b7844763ca31fe0e95633ac4f3519314c999adc
Mario García Martín <mgarcia@visual-engin.com>
2016-11-23 09:00:27 +0100

    Basic test

---
+++ test/1.basic/index.spec.js
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+
+import sut from '../../src/1.basic';
+
+describe('Basic', function() {
+	xit('Should exist', function() {
+
+	});
+
+	xit('Should be able to create a new instance', function() {
+
+	});
+
+	xit('Should be able to initialize the instance fields', function() {
+
+	});
+
+	xit('Should be able to retrieve the full name', function() {
+
+	});
+});
