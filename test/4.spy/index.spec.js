a50f0bc9366bfb23bc0336823e08add26282d3b9
Mario García Martín <mgarcia@visual-engin.com>
2016-11-23 09:10:02 +0100

    Spy test

---
+++ test/4.spy/index.spec.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import action from '../../src/4.spy';
+
+describe('Spy', function() {
+	xit('Should exist', function() {
+
+	});
+
+	xit('Should execute function', function() {
+
+	});
+});
