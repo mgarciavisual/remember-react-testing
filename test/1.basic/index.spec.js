d348029bc8cb839bf9e098d70865dd3650926ee0
Mario García Martín <mgarcia@visual-engin.com>
2016-12-09 10:56:40 +0100

    Tests for basic module

--- test/1.basic/index.spec.js
+++ test/1.basic/index.spec.js
@@ -3,19 +3,32 @@ import { expect } from 'chai';
 import Person from '../../src/1.basic';
 
 describe('Basic', function() {
-	xit('Should exist', function() {
+	it('Should exist', function() {
+		expect(Person).to.exist;
 
+		expect(Person).not.to.equal(undefined);
+		expect(Person).not.to.equal(null);
+
+		expect(Person).to.be.a('function');
 	});
 
-	xit('Should be able to create a new instance', function() {
+	it('Should be able to create a new instance', function() {
+		const homer = new Person();
 
+		expect(homer).to.exist;
+		expect(homer).to.be.an('object');
 	});
 
-	xit('Should be able to initialize the instance fields', function() {
+	it('Should be able to initialize the instance fields', function() {
+		const homer = new Person('Homer', 'Simpson');
 
+		expect(homer.name).to.equals('Homer');
+		expect(homer.surname).to.equals('Simpson');
 	});
 
-	xit('Should be able to retrieve the full name', function() {
+	it('Should be able to retrieve the full name', function() {
+		const homer = new Person('Homer', 'Simpson');
 
+		expect(homer.getFullName()).to.equals('Homer Simpson');
 	});
 });
