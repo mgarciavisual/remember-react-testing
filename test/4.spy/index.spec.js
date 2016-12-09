82b847766e39d7ff7156614b32908f37839bad07
Mario García Martín <mgarcia@visual-engin.com>
2016-12-09 10:57:32 +0100

    Test for spy module

--- test/4.spy/index.spec.js
+++ test/4.spy/index.spec.js
@@ -4,11 +4,34 @@ import sinon from 'sinon';
 import action from '../../src/4.spy';
 
 describe('Spy', function() {
-	xit('Should exist', function() {
-
+	it('Should exist', function() {
+		expect(action).to.exist;
 	});
 
-	xit('Should execute function', function() {
+	it('Should execute function', function() {
+		let called = false,
+			param;
 
+		action(function(p) {
+			called = true;
+			param = p;
+		});
+
+		expect(called).to.equals(true);
+		expect(param).to.equal(1);
+	});
+
+	it('Should execute function (spy)', function() {
+		let spy = sinon.spy();
+
+		sinon.spy(Math, 'cos');
+
+		action(spy);
+		expect(spy.callCount).to.equals(1);
+		expect(spy.calledWith(1)).to.equals(true);
+		expect(Math.cos.callCount).to.equals(1);
+		expect(Math.cos.calledWith(0)).to.equals(true);
+
+		Math.cos.restore();
 	});
 });
