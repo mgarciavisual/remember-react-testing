6e82db228dfe6269aee279ca543f2553d1c0befc
Mario García Martín <mgarcia@visual-engin.com>
2016-12-09 10:57:42 +0100

    Test for stub module

--- test/5.stub/index.spec.js
+++ test/5.stub/index.spec.js
@@ -4,19 +4,38 @@ import sinon from 'sinon';
 import openWindow from '../../src/5.stub';
 
 describe('Stub', function() {
-	xit('Should exist', function() {
-
+	beforeEach(function() {
+		sinon.stub(window, 'open');
 	});
 
-	xit('Should open a URL with provided URL and title', function() {
-
+	afterEach(function() {
+		window.open.restore();
 	});
 
-	xit('Should open a URL with the default title', function() {
-
+	it('Should exist', function() {
+		expect(openWindow).to.exit;
 	});
 
-	xit('Should do nothing if no URL provided', function() {
+	it('Should open a URL with the provided URL and title', function() {
+		openWindow('www.zara.com', 'A totally random URL');
+		expect(window.open.callCount).to.equals(1);
+		
+		let args = window.open.lastCall.args;
+		expect(args[0]).to.equals('www.zara.com');
+		expect(args[1]).to.equals('A totally random URL');
+	});
 
+	it('Should open a URL with the default title', function() {
+		openWindow('www.zara.com');
+		expect(window.open.callCount).to.equals(1);
+		
+		let args = window.open.lastCall.args;
+		expect(args[0]).to.equals('www.zara.com');
+		expect(args[1]).to.equals('');
+	});
+
+	it('Should do nothing if no URL provided', function() {
+		openWindow();
+		expect(window.open.callCount).to.equals(0);
 	});
 });
