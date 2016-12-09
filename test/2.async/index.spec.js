62146460740e9aa07fe1e3e18388c0f15eadb8ee
Mario García Martín <mgarcia@visual-engin.com>
2016-12-09 10:56:51 +0100

    Tests for async module

--- test/2.async/index.spec.js
+++ test/2.async/index.spec.js
@@ -3,19 +3,49 @@ import { expect } from 'chai';
 import delayedRemove from '../../src/2.async';
 
 describe('Async', function() {
-	xit('Should exist', function() {
-
+	it('Should exist', function() {
+		expect(delayedRemove).to.exist;
 	});
 
-	xit('Should remove the last element after 50ms', function(done) {
+	it('Should remove the last element after 50ms', function(done) {
+		let items = [1, 2, 3];
 
+		delayedRemove(items, 50);
+		setTimeout(() => {
+			expect(items).to.have.lengthOf(2);
+			done();
+		}, 60);
 	});
 
-	xit('Should remove the last element after 1500ms', function(done) {
-	
+	it('Should remove the last element after 1500ms', function(done) {
+		let items = [1, 2, 3];
+
+		delayedRemove(items, 1500);
+		setTimeout(() => {
+			expect(items).to.have.lengthOf(2);
+			done();
+		}, 1510);
 	});
 
-	xit('Should remove the last element after default timeout value', function(done) {
-	
+	it('Should remove the last element after 2500ms', function(done) {
+		this.timeout(3000);
+
+		let items = [1, 2, 3];
+
+		delayedRemove(items, 2500);
+		setTimeout(() => {
+			expect(items).to.have.lengthOf(2);
+			done();
+		}, 2510);
+	});
+
+	it('Should remove the last element after default timeout value', function(done) {
+		let items = [1, 2, 3];
+
+		delayedRemove(items);
+		setTimeout(() => {
+			expect(items).to.have.lengthOf(2);
+			done();
+		}, 160);
 	});
 });
