147fb177ba2721a7c667c6b935e62fedeeb281a1
Mario García Martín <mgarcia@visual-engin.com>
2016-12-09 10:57:17 +0100

    Tests for fake timer module

--- test/3.fake-timer/index.spec.js
+++ test/3.fake-timer/index.spec.js
@@ -4,19 +4,41 @@ import sinon from 'sinon';
 import delayedRemove from '../../src/3.fake-timer';
 
 describe('Fake timer', function() {
-	xit('Should exist', function() {
+	let clock;
 
+	beforeEach(function() {
+		clock = sinon.useFakeTimers();
 	});
 
-	xit('Should remove the last element after 50ms', function() {
-
+	afterEach(function() {
+		clock.restore();
 	});
 
-	xit('Should remove the last element after 1500ms', function() {
-	
+	it('Should exist', function() {
+		expect(delayedRemove).to.exist;
 	});
 
-	xit('Should remove the last element after default timeout value', function() {
-	
+	it('Should remove the last element after 50ms', function() {
+		let items = [1, 2, 3];
+
+		delayedRemove(items, 50);
+		clock.tick(51);
+		expect(items).to.have.lengthOf(2);
+	});
+
+	it('Should remove the last element after 1500ms', function() {
+		let items = [1, 2, 3];
+
+		delayedRemove(items, 1500);
+		clock.tick(1501);
+		expect(items).to.have.lengthOf(2);
+	});
+
+	it('Should remove the last element after default timeout value', function() {
+		let items = [1, 2, 3];
+
+		delayedRemove(items);
+		clock.tick(151);
+		expect(items).to.have.lengthOf(2);
 	});
 });
